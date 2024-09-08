f265d8a598aac60db7174b3e2f12e99ae6b04a82
woranov <wor4nov+github@gmail.com>
2024-09-08 23:09:33 +0200

    add lazy loading LazyDocBin
    also introduces basic logging

--- src/spacy_lazy_docbin/__init__.py
+++ src/spacy_lazy_docbin/__init__.py
@@ -1,8 +1,10 @@
 from .base import DocBin
+from .lazy import LazyDocBin
 from .utils import docbin_sentence_counts
 
 
 __all__ = [
     "DocBin",
+    "LazyDocBin",
     "docbin_sentence_counts",
 ]
---
+++ src/spacy_lazy_docbin/lazy.py
@@ -0,0 +1,48 @@
+from os import PathLike
+from pathlib import Path
+from spacy_lazy_docbin.logging import logger
+from spacy_lazy_docbin.base import DocBin, BaseDocBin
+
+
+class LazyDocBin(DocBin):
+    def __init__(self, *args, path: PathLike, **kwargs):
+        self.path = Path(path)
+        self._loaded = False
+        super().__init__(*args, **kwargs)
+
+    def _load(self):
+        if not self._loaded:
+            if self.path.exists():
+                # we allow for docs being added before loading the file
+                if self.tokens:
+                    existing_docs_bin = BaseDocBin(
+                        attrs=self.attrs,
+                        store_user_data=self.store_user_data,
+                        docs=self.get_docs(),
+                    )
+                else:
+                    existing_docs_bin = None
+
+                logger.info(f"Loading DocBin from {self.path}")
+                self.from_disk(self.path)
+                if existing_docs_bin:
+                    self.merge(existing_docs_bin)
+            else:
+                super().to_disk(self.path)
+
+            self._loaded = True
+
+    def save_and_unload(self):
+        self.to_disk(self.path)
+        self.tokens = []
+        self.spaces = []
+        self.cats = []
+        self.span_groups = []
+        self.user_data = []
+        self.flags = []
+        self.strings = set()
+        self._loaded = False
+
+    def to_disk(self, path: PathLike):
+        self._load()
+        super().to_disk(Path(path))
